7314da090ce13e4775a35237163e7348803dbdbc
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-24 12:59:54 +0530

    Create printMessage.h

---
+++ printMessage.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+bool displayTemperatureGerman(int language){
+  if(language == 2){
+    printf("Temperatur außerhalb des zulässigen Bereichs!\n");
+    return true;
+  }
+  //addition of language is possible in future here
+}
+
+bool dispalyTemperature(int language){
+  if(language == 1){
+    printf("Temperature out of range!\n");
+    return true;
+  }
+  displayTemperatureGerman(language);
+}
