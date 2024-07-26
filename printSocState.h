392ae8c2bad17a87d16b3e9354619307fd4ccf7c
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-26 09:06:24 +0530

    Update printSocState.h

--- printSocState.h
+++ printSocState.h
@@ -1,1 +1,6 @@
-
+bool dispalyTemperature(int language);
+bool displayTemperatureGerman(int language);
+bool displaySocGerman(int language);
+bool displaySoc(int language);
+bool displayChargeRateGerman(int language);
+bool displayChargeRate(int language);
