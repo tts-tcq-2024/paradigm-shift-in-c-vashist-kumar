48367d82c48900d1b0d043309e4ecc9ca6eb4727
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-26 09:06:14 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdbool.h>
-bool dispalyTemperature(int language);
-bool displayTemperatureGerman(int language);
-bool displaySocGerman(int language);
-bool displaySoc(int language);
-bool displayChargeRateGerman(int language);
-bool displayChargeRate(int language);
+
 
 bool displayTemperatureGerman(int language){
   if(language == 2){
