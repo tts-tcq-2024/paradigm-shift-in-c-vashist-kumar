84ee6b961c9e7c05839cd61a9edf36f59ab296c9
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-26 09:04:22 +0530

    Update printSocState.h

--- printSocState.h
+++ printSocState.h
@@ -1,19 +1,1 @@
-#include <stdio.h>
-#include <stdbool.h>
-bool dispalySoc(int language);
 
-bool displaySocGerman(int language){
-  if(language == 2){
-    printf("Ladezustand außerhalb des Bereichs!\n");
-    return true;
-  }
-  return false;//addition of language is possible in future here
-}
-
-bool dispalySoc(int language){
-  if(language == 1){
-    printf("State of Charge out of range!\n");
-    return true;
-  }
-  displaySocGerman(language);
-}
