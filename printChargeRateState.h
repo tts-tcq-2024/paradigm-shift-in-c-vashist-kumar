1bdd1c863d7aa7246052bb5ca160cf836889ca26
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-26 09:04:10 +0530

    Update printChargeRateState.h

--- printChargeRateState.h
+++ printChargeRateState.h
@@ -1,19 +1,1 @@
-#include <stdio.h>
-#include <stdbool.h>
-bool dispalyChargeRate(int language);
 
-bool displayChargeRateGerman(int language){
-  if(language == 2){
-    printf("Laderate außerhalb des Bereichs!\n");
-    return true;
-  }
-  return false;//addition of language is possible in future here
-}
-
-bool dispalyChargeRate(int language){
-  if(language == 1){
-    printf("Charge Rate out of range!\n");
-    return true;
-  }
-  displayChargeRateGerman(language);
-}
