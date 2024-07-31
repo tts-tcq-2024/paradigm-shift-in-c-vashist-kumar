f54e5aa9c83ce4fe27e7cdabd53f4a109b34a570
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-31 12:26:20 +0530

    Update temperatureCheck.h

--- temperatureCheck.h
+++ temperatureCheck.h
@@ -1,1 +1,7 @@
-
+int tempcheck(float temperature,int language){
+    if(temperature < 0 || temperature > 45){
+        temperatureStateMessage(language);
+        return 0;
+    }
+    return 1;
+}
