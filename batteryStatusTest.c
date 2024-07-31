da5c99352e9c94cda04d39540054ccd0ac92b2d4
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-31 12:29:26 +0530

    Update batteryStatusTest.c

--- batteryStatusTest.c
+++ batteryStatusTest.c
@@ -4,7 +4,7 @@
 #include "temperatureStateMessage.h"
 
 int batteryIsOk(float temperature, float soc, float chargeRate, int language) {
-   return tempcheck(temperature,language) && soccheck(soc,language) && chargecheck(chargeRate,language);
+   return tempcheck(temperature,language) && soccheck(soc,language) && chargeRatecheck(chargeRate,language);
 }
 
 int main() {
