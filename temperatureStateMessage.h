9215536ea4bdd12ff78aec83bf1ce707e2d49233
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-31 12:45:26 +0530

    Update temperatureStateMessage.h

--- temperatureStateMessage.h
+++ temperatureStateMessage.h
@@ -2,7 +2,7 @@
 
 bool temperatureStateGerman(int language){
   if(language == 2){
-    printf("Temperatur außerhalb des zulässigen Bereichs!\n");
+    printMessage("Temperatur außerhalb des zulässigen Bereichs!");
     return true;
   }
   return false;//addition of language is possible in future here
@@ -10,7 +10,7 @@ bool temperatureStateGerman(int language){
 
 bool temperatureStateMessage(int language){
   if(language == 1){
-    printf("Temperature out of range!\n");
+    printMessage("Temperature out of range!");
     return true;
   }
   return temperatureStateGerman(language);
