7b56f42f066bfe6a1dc9a5c930e0acba4ee190d4
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-31 12:14:22 +0530

    Update temperatureStateMessage.h

--- temperatureStateMessage.h
+++ temperatureStateMessage.h
@@ -14,21 +14,7 @@ bool dispalyTemperature(int language){
   return displayTemperatureGerman(language);
 }
 
-bool displaySocGerman(int language){
-  if(language == 2){
-    printf("Ladezustand außerhalb des Bereichs!\n");
-    return true;
-  }
-  return false;//addition of language is possible in future here
-}
 
-bool displaySoc(int language){
-  if(language == 1){
-    printf("State of Charge out of range!\n");
-    return true;
-  }
-  return displaySocGerman(language);
-}
 
 bool displayChargeRateGerman(int language){
   if(language == 2){
