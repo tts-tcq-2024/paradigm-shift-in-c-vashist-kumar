7833d182954e65d8d7f42a1964041dd55d1bdf04
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-31 12:15:01 +0530

    Update chargeRateStateMessage.h

--- chargeRateStateMessage.h
+++ chargeRateStateMessage.h
@@ -1,1 +1,15 @@
+bool displayChargeRateGerman(int language){
+  if(language == 2){
+    printf("Laderate außerhalb des Bereichs!\n");
+    return true;
+  }
+  return false;//addition of language is possible in future here
+}
 
+bool displayChargeRate(int language){
+  if(language == 1){
+    printf("Charge Rate out of range!\n");
+    return true;
+  }
+  return displayChargeRateGerman(language);
+}
