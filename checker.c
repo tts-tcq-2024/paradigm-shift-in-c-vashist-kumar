2fd55a4ad51e0ac0f3e2eecbf05071bd6be6c84e
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-31 09:41:48 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -8,7 +8,7 @@
 int batteryIsOk(float temperature, float soc, float chargeRate, int language) {
    return tempcheck(temperature,language) && soccheck(soc,language) && chargecheck(chargeRate,language);
 }
-
+//yes
 int main() {
     assert(batteryIsOk(25, 70, 0.7,1));
     assert(batteryIsOk(25, 70, 0.7,2));
