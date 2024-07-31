33fa9bc4fb0912a4ac95f2df114bfec775f35774
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-31 11:19:49 +0530

    Update and rename checker.c to batteryStatusTest.c

--- batteryStatusTest.c
+++ batteryStatusTest.c
@@ -3,12 +3,10 @@
 #include <stdbool.h>
 #include "chargeRateStateMessage.h"
 
-
-
 int batteryIsOk(float temperature, float soc, float chargeRate, int language) {
    return tempcheck(temperature,language) && soccheck(soc,language) && chargecheck(chargeRate,language);
 }
-//yes
+
 int main() {
     assert(batteryIsOk(25, 70, 0.7,1));
     assert(batteryIsOk(25, 70, 0.7,2));
