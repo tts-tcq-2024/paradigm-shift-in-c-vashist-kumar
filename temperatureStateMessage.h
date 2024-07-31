21fafb04bbdc948aba08a18fa880b65ed223f779
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-31 12:27:44 +0530

    Update temperatureStateMessage.h

--- temperatureStateMessage.h
+++ temperatureStateMessage.h
@@ -20,11 +20,3 @@ bool temperatureStateMessage(int language){
 
 
 
-int chargecheck(float chargeRate,int language){
-    if(chargeRate > 0.8){
-         chargeRateStateMessage(language);
-        return 0;
-    }
-    return 1;
-}
-
