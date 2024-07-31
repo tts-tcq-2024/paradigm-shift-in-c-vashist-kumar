16f7c439aa401b867a8a2dcd1aff6dfc78cc633b
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-31 12:14:30 +0530

    Update socStateMessage.h

--- socStateMessage.h
+++ socStateMessage.h
@@ -1,1 +1,15 @@
+bool displaySocGerman(int language){
+  if(language == 2){
+    printf("Ladezustand außerhalb des Bereichs!\n");
+    return true;
+  }
+  return false;//addition of language is possible in future here
+}
 
+bool displaySoc(int language){
+  if(language == 1){
+    printf("State of Charge out of range!\n");
+    return true;
+  }
+  return displaySocGerman(language);
+}
