3c69768dc8ac7a005723b260c381b7d3379ebf27
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-08-04 17:39:43 +0530

    Update chargeRateStateMessage.h

--- chargeRateStateMessage.h
+++ chargeRateStateMessage.h
@@ -1,17 +1,4 @@
-
-
-bool chargeRateStateGerman(int language){
-  if(language == 2){
-    printMessage("Laderate außerhalb des Bereichs!\n");
-    return true;
-  }
-  return false;//addition of language is possible in future here
-}
-
 bool chargeRateStateMessage(int language){
-  if(language == 1){
-    printMessage("Charge Rate out of range!\n");
-    return true;
-  }
-  return chargeRateStateGerman(language);
+  printMessage(&chargeRateMessage[language]);
+  return true;
 }
