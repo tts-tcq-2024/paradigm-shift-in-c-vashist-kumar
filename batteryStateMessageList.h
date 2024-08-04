53c39853ab177cf7b8ae01626bcdefc6baf2bfc3
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-08-04 15:18:56 +0530

    Update batteryStateMessageList.h

--- batteryStateMessageList.h
+++ batteryStateMessageList.h
@@ -1,5 +1,11 @@
-const char *temperatureStateMessages[] = {
+const char *batteryStateMessageEnglish[] = {
 "Temperature out of range!",
-"",
-""
+"State of Charge out of range!",
+"Charge Rate out of range!"
+}
+
+const char *batteryStateMessageGerman[] = {
+"Temperatur außerhalb des zulässigen Bereichs!",
+"Ladezustand außerhalb des Bereichs!",
+"Laderate außerhalb des Bereichs!"
 }
