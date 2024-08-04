df25efcb313fe858d108f93bfb5f628924c3ddf5
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-08-04 18:09:47 +0530

    Update batteryStatusTest.c

--- batteryStatusTest.c
+++ batteryStatusTest.c
@@ -15,5 +15,5 @@ int main() {
     assert(!batteryIsOk(50, 85, 0,1));
     assert(!batteryIsOk(36, 85, 0.8,1));
     assert(!batteryIsOk(50, 46, 0,2));
-    assert(!batteryIsOk(16, 44, 0.5,1));
+    assert(batteryIsOk(16, 44, 0.5,1));
 }
