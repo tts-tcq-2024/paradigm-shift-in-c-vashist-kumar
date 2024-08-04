97d68b7aa255b3e38065094a8287af5ce263e144
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-08-04 17:53:22 +0530

    Update socStateCheck.h

--- socStateCheck.h
+++ socStateCheck.h
@@ -2,7 +2,7 @@
 
 int soccheck(float soc,int language){
     if(soc < 20 || soc > 80){
-        socStateMessage(language);
+        printMessage(socMessage[language]);
         return 0;
     }
     return 1;
