931268670afdbe0d778d3c24847c41c4e34ef246
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-08-06 21:48:43 +0530

    Update testPrintMessageUtils.h

--- testPrintMessageUtils.h
+++ testPrintMessageUtils.h
@@ -1,8 +1,10 @@
+
+
 void testPrintColorMap() {
     char buffer[2048];
     memset(buffer, 0, sizeof(buffer));
     setvbuf(stdout, buffer, _IOFBF, sizeof(buffer));
-
+    printMessageMock("Hello World");
     fflush(stdout);
 
     const char* expectedOutput = "Hello World!"
