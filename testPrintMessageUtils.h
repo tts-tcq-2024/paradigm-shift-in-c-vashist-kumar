dc0db89503e2b85d42c5af2f6779fbc56720beca
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-08-06 21:46:46 +0530

    Update testPrintMessageUtils.h

--- testPrintMessageUtils.h
+++ testPrintMessageUtils.h
@@ -2,8 +2,7 @@ void testPrintColorMap() {
     char buffer[2048];
     memset(buffer, 0, sizeof(buffer));
     setvbuf(stdout, buffer, _IOFBF, sizeof(buffer));
-    
-    int result = printColorMap();
+
     fflush(stdout);
 
     const char* expectedOutput = "Hello World!"
